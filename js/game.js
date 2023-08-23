8a7d545d1ba4327169d5dc562f6764ab3434b0ea
siangjyunsyu <siangjyun0926@gmail.com>
2023-08-23 22:22:18 +0800

    開始畫面完成，JS尚未完成

---
+++ js/game.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+  console.log('hello');
+  let gameStart = document.getElementById('gameStart');
+
+  gameStart.addEventListener('click', () => {
+    console.log('OK');
+  });
+};
